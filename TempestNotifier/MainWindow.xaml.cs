7b464aacb99c892f6382ea293c004bacbe01d0db
Rasmus Karlsson <pajlada@bithack.se>
2015-07-22 18:45:51 +0200

    Enabled the timer, made some visual improvements.

--- TempestNotifier/MainWindow.xaml.cs
+++ TempestNotifier/MainWindow.xaml.cs
@@ -34,36 +34,54 @@ namespace TempestNotifier
     {
         public string name { get; set; }
 
-        public Tempest tempest { get; set; }
+        public string tempest { get; set; }
+
+        public bool good { get; set; }
+    }
+
+    class TempestDescription
+    {
+        public string short_description { get; set; }
+
+        public bool good { get; set; }
     }
 
     public partial class MainWindow : Window
     {
         Timer timer;
-        List<String> relevant_tempests = new List<String>();
+        Dictionary<String, TempestDescription> relevant_tempests;
+        Dictionary<String, int> map_levels;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            relevant_tempests.Add("abyssal");
-            relevant_tempests.Add("shining");
-            relevant_tempests.Add("radiating");
-            relevant_tempests.Add("stinging");
-            relevant_tempests.Add("scathing");
-            relevant_tempests.Add("corrupting");
-            relevant_tempests.Add("veiling");
-            relevant_tempests.Add("destiny");
-            relevant_tempests.Add("refining");
-            relevant_tempests.Add("turmoil");
-            relevant_tempests.Add("revelation");
-            relevant_tempests.Add("phantoms");
-            relevant_tempests.Add("animation");
-            relevant_tempests.Add("inspiration");
+            map_levels = new Dictionary<string, int>
+            {
+                { "crypt", 68 }, { "desert", 68 }, { "dunes", 68 }, { "dungeon", 68 }, { "grotto", 68 }, { "pit", 68 }, { "tropical_island", 68 }, { "aqueduct", 69 }, { "arcade", 69 }, { "cemetery", 69 }, { "channel", 69 }, { "mountain_ledge", 69 }, { "sewer", 69 }, { "thicket", 69 }, { "wharf", 69 }, { "ghetto", 70 }, { "mud_geyser", 70 }, { "museum", 70 }, { "quarry", 70 }, { "reef", 70 }, { "spider_lair", 70 }, { "vaal_pyramid", 70 }, { "arena", 71 }, { "overgrown_shrine", 71 }, { "promenade", 71 }, { "shore", 71 }, { "spider_forest", 71 }, { "tunnel", 71 }, { "bog", 72 }, { "coves", 72 }, { "graveyard", 72 }, { "pier", 72 }, { "underground_sea", 72 }, { "villa", 72 }, { "arachnid_nest", 73 }, { "catacomb", 73 }, { "colonnade", 73 }, { "dry_woods", 73 }, { "strand", 73 }, { "temple", 73 }, { "jungle_valley", 74 }, { "labyrinth", 74 }, { "mine", 74 }, { "torture_chamber", 74 }, { "waste_pool", 74 }, { "canyon", 75 }, { "cells", 75 }, { "dark_forest", 75 }, { "dry_peninsula", 75 }, { "orchard", 75 }, { "arid_lake", 76 }, { "gorge", 76 }, { "residence", 76 }, { "underground_river", 76 }, { "abyss", 77 }, { "bazaar", 77 }, { "necropolis", 77 }, { "plateau", 77 }, { "academy", 78 }, { "crematorium", 78 }, { "precinct", 78 }, { "springs", 78 }, { "arsenal", 79 }, { "overgrown_ruin", 79 }, { "shipyard", 79 }, { "village_ruin", 79 }, { "courtyard", 80 }, { "excavation", 80 }, { "wasteland", 80 }, { "waterways", 80 }, { "maze", 81 }, { "palace", 81 }, { "shrine", 81 }, { "vaal_temple", 81 }, { "colosseum", 82 }, { "core", 82 }, { "volcano", 82 }
+            };
+
+            relevant_tempests = new Dictionary<string, TempestDescription>
+            {
+                { "abyssal", new TempestDescription { short_description = "Chaos damage", good = false } },
+                { "shining", new TempestDescription { short_description = "Increased item Rarity/Quantity", good = true } },
+                { "radiating", new TempestDescription { short_description = "Increased item Rarity/Quantity", good = true } },
+                { "stinging", new TempestDescription { short_description = "All hits are Critical Strikes", good = false } },
+                { "scathing", new TempestDescription { short_description = "All hits are Critical Strikes", good = false } },
+                { "corrupting", new TempestDescription { short_description = "Corrupted drops", good = true } },
+                { "veiling", new TempestDescription { short_description = "0% elemental resists", good = false } },
+                { "destiny", new TempestDescription { short_description = "1 guaranteed map drop", good = true } },
+                { "refining", new TempestDescription { short_description = "Quality drops", good = true } },
+                { "turmoil", new TempestDescription { short_description = "20 additional rogue exiles", good = true } },
+                { "revelation", new TempestDescription { short_description = "50% increased experience", good = true } },
+                { "phantoms", new TempestDescription { short_description = "10 additional tormented spirits", good = true } },
+                { "animation", new TempestDescription { short_description = "Weapons are animated", good = false } },
+                { "inspiration", new TempestDescription { short_description = "15% increased experience", good = true } },
+            };
 
             timer = new System.Timers.Timer(Convert.ToInt32(TimeSpan.FromMinutes(2).TotalMilliseconds));
             timer.Elapsed += Timer_Elapsed;
-            timer.Enabled = false;
+            timer.Enabled = true;
 
             update_tempests();
         }
@@ -100,13 +118,31 @@ namespace TempestNotifier
                     {
                         await Dispatcher.BeginInvoke(new Action(() =>
                         {
-                            foreach (string tt in this.relevant_tempests)
+                            string tempest_string = "";
+                            var map_name = String.Format("{0} ({1})", kv.Key, this.map_levels[kv.Key]);
+                            bool good = true;
+
+                            foreach (KeyValuePair<string, TempestDescription> relevant_tempest in this.relevant_tempests)
                             {
-                                if (kv.Value.name.ToLower().Contains(tt))
+                                if (kv.Value.name.ToLower().Contains(relevant_tempest.Key))
                                 {
-                                    this.listview_maps.Items.Add(new Map { name = kv.Key, tempest = kv.Value });
-                                    break;
+                                    if (tempest_string.Length > 0)
+                                    {
+                                        tempest_string += ", ";
+                                    }
+                                    tempest_string += relevant_tempest.Value.short_description;
+                                    if (relevant_tempest.Value.good == false)
+                                    {
+                                        good = false;
+                                    }
                                 }
+                            }
+
+                            if (tempest_string.Length > 0)
+                            {
+                                int x = this.listview_maps.Items.Add(new Map { name = map_name, tempest = tempest_string, good = good });
+                                //this.listview_maps.Items.
+                                Console.WriteLine(String.Format("Added a map, got this in return: {0}", x));
                             }
                         }));
                     }
@@ -124,9 +160,9 @@ namespace TempestNotifier
             }));
         }
 
-        private void button_Click_1(object sender, RoutedEventArgs e)
+        private async void button_Click_1(object sender, RoutedEventArgs e)
         {
-            update_tempests();
+            await update_tempests();
         }
     }
 }
